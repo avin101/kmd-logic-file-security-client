76e8c4fde2388f600b92594acfb8c54e1a8ad0e4
Avin Mathew (UYW) <avin101@gmail.com>
2021-05-14 18:17:23 +0530

    initial push with a new proj to generate test doc with privileges

---
+++ Kmd.Logic.FileSecurity.Client.DocumentPrivilegesSample/AppConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Kmd.Logic.Identity.Authorization;
+
+namespace Kmd.Logic.FileSecurity.Client.DocumentPrivilegesSample
+{
+    /// <summary>
+    /// Class to get configuration details.
+    /// </summary>
+    internal class AppConfiguration
+    {
+        /// <summary>
+        /// Gets or sets authorization token.
+        /// </summary>
+        public LogicTokenProviderOptions TokenProvider { get; set; } = new LogicTokenProviderOptions();
+
+        /// <summary>
+        /// Gets or sets filesecurity configuration options.
+        /// </summary>
+        public FileSecurityOptions FileSecurityOptions { get; set; } = new FileSecurityOptions();
+    }
+}
